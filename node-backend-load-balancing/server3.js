1bfc0099192f020362ef7057b0117bdd06a96fb3
Mushfiqueghayas <mushfiqueshaikh69@gmail.com>
2025-09-28 00:54:10 +0530

    Update server3.js

--- node-backend-load-balancing/server3.js
+++ node-backend-load-balancing/server3.js
@@ -1,4 +1,5 @@
 const http = require('http');
-http.createServer((req, res) => {
- res.end('✅Hello This is MUSHFIQUE GHAYAS from Node.js backend SERVICE-1 SERVER-2 !!!!');
-}).listen(3002);
+  http.createServer((req, res) => {
+   res.end('✅Hello This is MUSHFIQUE GHAYAS from Node.js backend SERVICE-1 SERVER-2 !!!!');
+  
+  }).listen(3002);
